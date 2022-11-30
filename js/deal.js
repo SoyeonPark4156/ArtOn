55d17a064ef8a8a782093fbf60478b283ee62775
SoyeonPark4156 <soy_4156@naver.com>
2022-11-30 16:13:07 +0900

    Update deal.js

--- js/deal.js
+++ js/deal.js
@@ -44,6 +44,9 @@ $(document).ready(function(){
         }
         else if(header_bot <= tab_o_top){
             $('.tabs').removeClass('tab_event')
+            $('#wrap').css({
+                paddingTop : 0
+            })
         };
 
     });
@@ -86,18 +89,69 @@ $(document).ready(function(){
         $('#text_counter').text(content_cnt);
     });
 
+    //리뷰 작성칸에 사용자가 마우스 클릭하면 로그인된 회원만 후기 작성 가능하다고 알람띄우기
+    $('#review_content').click(function(){
+        alert("로그인한 회원만 작성가능합니다.")
+        
+    });
+
     //리뷰탭 - 후기 작성시 유효성 검사
+    // rv_content => 예매한 사람만 작성할 수 있다고 alert 띄워주기.
+    // 등록하기 할 때 디비 연동해서 유효성 검사할 것.
+
+    //리뷰 등록하기
     $('#btn_submit_review').click(function(){
+
         let content_cnt = $('#review_content').val().length;
+        let content = $('#review_content').val();
+        let date = new Date();
+        let today = `${date.getFullYear()}-${date.getMonth()}-${date.getDay()}`;
+
         if(content_cnt <= 10){
-            alert("10자 이상 적어주세요!")
+            alert("10자 이상 입력해주세요!")
         }
         else if(content_cnt >= 500){
             alert("최대 500자까지 입력 가능합니다.")
         }
-
-    alert("후기가 등록되었습니다.")
+        else{
+            alert("후기가 등록되었습니다.")
+        
+        $('.review_list').append(
+                                `<div class="rv_list_box">
+                                    <div class="rv_user_id">soy_4156</div>
+                                    <div class="rv_uploaded_box">
+                                        <div class="rv_uploaded"><span id="rv_upload_date">${today}</span></div>
+                                        <div class="rv_uploaded"><span id="rv_uploaded_content">${content}</span></div>
+                                    </div>
+                                    <div class="rv_right_box">
+                                        <div class="rv_star_rating">★★★★★</div>
+                                        <div class="rv_modify_delete">
+                                            <div class="rv_modify_btn">수정</div> |
+                                            <div class="rv_delete_btn">삭제</div>
+                                        </div>
+                                    </div>
+                                </div>`
+        );
+        }
     });
+
+
+    //리뷰 리스트에서 리뷰 "수정" 버튼 누르면 수정할 수 있는 폼 보이게 하기
+    $('.rv_modify_btn').click(function(){
+        let rv_box = $(this).parents('.rv_list_box');
+        
+
+        rv_box.css({
+            display : 'none'
+        })
+        $('.rv_modify_box').css({
+            display : 'block'
+        });
+
+    })
+
+    
+
 });
 
 

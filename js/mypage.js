b4c1f44f95c88469d8d75b38eb4b6e97b3429cfc
SoyeonPark4156 <soy_4156@naver.com>
2022-12-03 06:30:51 +0900

    Update mypage.js

--- js/mypage.js
+++ js/mypage.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
 
+   
+
     //헤더 따라다니게 하기
         $(window).scroll(function(){
             let header_bot = $(window).scrollTop();
@@ -17,6 +19,31 @@ $(document).ready(function(){
                 })
             }
         });
+
+        //탭!!!!!!!!
+        $('.mytabs_li').click(function(){
+
+            let tab_id = $(this).attr('data-tab');
+    
+            $('.mypage_tab').removeClass('tab_curr');
+            $("#"+tab_id).addClass('tab_curr');
+    
+            $(this).css({
+                color:'#008049'
+            })
+            $(this).siblings().css({
+                color: '#000'
+            })
+    
+            // $(this).siblings().children('span').css({
+            //     display:'none'
+            // })
+            // $(this).children('span').css({
+            //     display:'block'
+            // })
+            
+        });
+        $('li[data-tab="mytab_tit_chk"]').trigger('click');
 
 
     let myticket = `<tr>
@@ -48,35 +75,45 @@ $(document).ready(function(){
                         <td class="status">예매완료(입금완료)</td>
                     </tr>`
 
-    //예매목록 5개만 넣어놓기
-    for(let i = 0;i<4;i++){
-        $('.mytit_list_tbody').append(myticket);
+    //예매목록 3개만 넣어놓기
+    for(let i = 0;i<2;i++){
+        $('.tab1').append(myticket);
     }
+    //예매확인탭에는 5개!!! 5개 이상부터는 페이지 나뉘어지게, 페이지네이션 활성화..이건 어떻게?
+    for(let i = 0;i<4;i++){
+        $('.tab2').append(myticket);
+    }
+    
 
-    //탭!!!!!!!
+    //예매내역이 있는 회원이라면 예매내역이 보이게 하고, 없는 회원이라면 "예매내역이 없습니다." 라는 멘트가 적힌 박스가 보이게
+    //이것도 물어봐야겠다~
+    $('#reserv_chk').click(function(){
+        $('.mytit_null_box span').text("예매")
 
-    $('.mytabs_li').click(function(){
+        $('.my_sort_btn').removeClass('zindex')
+        $(this).addClass('zindex')
 
-        let tab_id = $(this).attr('data-tab');
-
-		$('.mypage_tab').removeClass('tab_curr');
-        $("#"+tab_id).addClass('tab_curr');
-
-        $(this).css({
-            color:'#008049'
+        $('.mytit_null_box').css({
+            display : 'none'
         })
-        $(this).siblings().css({
-            color: '#000'
+        $('.mytit_list_table').css({
+            display : 'block'
         })
 
-        $(this).siblings().children('span').css({
-            width: '0'
-        })
-        $(this).children('span').css({
-            width : '100%'
-        })
-        
-	});
+    });
 
-    $('li[data-tab="mytab_mypage"]').trigger('click');
+    $('#reserv_cancel').click(function(){
+        $('.mytit_null_box span').text("취소")
+
+        $('.my_sort_btn').removeClass('zindex')
+        $(this).addClass('zindex')
+
+        $('.mytit_list_table').css({
+            display : 'none'
+        })
+        $('.mytit_null_box').css({
+            display : 'block'
+        })
+    });
+    
 });
